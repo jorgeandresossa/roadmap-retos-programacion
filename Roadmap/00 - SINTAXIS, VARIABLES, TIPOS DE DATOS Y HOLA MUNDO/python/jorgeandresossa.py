6db00b19ba9092dd158835aaf1b9486dd629e4a6
jorgeandresossa <jorgeandresossagonzalez@gmail.com>
2024-11-25 23:11:28 -0500

    Create jorgeandresossa.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jorgeandresossa.py
@@ -0,0 +1,22 @@
+# https://www.python.org/
+# Estoy aprendiendo a programar en Python 
+
+"""
+esto es 
+un comentario 
+en Python
+"""
+
+My_Variable = "Mi variable"
+My_Variable = "Mi nueva Variable"
+
+MY_CONSTANT = "Mi constante" # por conveción 
+
+my_int = 1
+my_float = 0.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_othher string = " Mi otra cadena de texto"
+
+print("¡hola, Python!")
